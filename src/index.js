9945e533b27dd1d69e334e3731bb501fc6885aca
Viktoriya de Sterke <v.de.sterke@gmail.com>
2025-09-05 19:07:16 +0200

    Implement prisma integration, authentication, and CRUD routes

--- src/index.js
+++ src/index.js
@@ -10,40 +10,35 @@ import * as Sentry from "@sentry/node";
 import * as Tracing from "@sentry/tracing";
 import errorHandlerSentry from "../middleware/errorHandlerSentry.js";
 
-
-
-
 const app = express();
 //Setry Intilize
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   release: "bookingapp@1.0.0", // required
   integrations: [
-    Sentry.httpIntegration({ tracing: true }),   // traces GET/POST/etc. at HTTP level
-    new Tracing.Integrations.Express({app}),          // traces Express routes
+    Sentry.httpIntegration({ tracing: true }), // traces GET/POST/etc. at HTTP level
+    new Tracing.Integrations.Express({ app }), // traces Express routes
   ],
   tracesSampleRate: 1.0, // 100% sampling; lower in production (e.g. 0.2)
   debug: false,
 });
 
-
 //Middleware
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
-app.use(log)
+app.use(log);
 app.use(express.json());
 
 //Routs
-app.use("/login", loginRouter)
-app.use("/users", usersRouter)
-app.use("/hosts", hostRouter)
-app.use("/properties", propertiesRouter)
-app.use("/bookings", bookingsRouter)
-app.use("/reviews", reviewsRouter)
+app.use("/login", loginRouter);
+app.use("/users", usersRouter);
+app.use("/hosts", hostRouter);
+app.use("/properties", propertiesRouter);
+app.use("/bookings", bookingsRouter);
+app.use("/reviews", reviewsRouter);
 
 //Error handlers
-app.use(errorHandlerSentry)
-
+app.use(errorHandlerSentry);
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
