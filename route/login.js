ab4650948e4d3dc82fb9c56a87ef40c59a11134a
Viktoriya de Sterke <v.de.sterke@gmail.com>
2025-09-29 20:35:17 +0200

    all test passed

--- route/login.js
+++ route/login.js
@@ -1,23 +1,19 @@
 import { Router } from "express";
-import { login } from "../services/loginServices";  
-
-
+import { login } from "../services/loginServices.js";
 
 const loginRouter = Router();
 
 loginRouter.post("/", async (req, res, next) => {
-  
   const { username, password } = req.body;
 
   try {
-    const { role, token} = await login(username, password)
-        return res.json({ message: `Succesfully logged in as a ${role}`, token });
-      
-    } catch (err) {
-      if (err.message === "Invalid credentials"){
-        return res.status(401).json({message: err.message})
-      }
-      next(err);
+    const { role, token } = await login(username, password);
+    return res.json({ message: `Succesfully logged in as a ${role}`, token });
+  } catch (err) {
+    if (err.message === "Invalid credentials") {
+      return res.status(401).json({ message: err.message });
+    }
+    next(err);
   }
 });
 
--- services/loginServices.js
+++ services/loginServices.js
@@ -6,43 +6,45 @@ const prisma = new PrismaClient();
 
 const secretKey = process.env.AUTH_SECRET_KEY || "my_secret_key";
 
-export const login = async(username, password)=>{
-    //check users
-    let account = await prisma.user.findUnique({ where: { username } });
-    if (account) {
-        if (account.password === password){
-            const token= jwt.sign({ id: account.id, type: "user" }, secretKey, { expiresIn: "1h" });
-            return {role: "user", token}
-        }
-        throw new Error("Invalid credentials")
+export const login = async (username, password) => {
+  //check users
+  let account = await prisma.user.findUnique({ where: { username } });
+  if (account) {
+    if (account.password === password) {
+      const token = jwt.sign({ id: account.id, type: "user" }, secretKey, {
+        expiresIn: "1h",
+      });
+      return { role: "user", token };
     }
+    throw new Error("Invalid credentials");
+  }
 
-    //check hosts
-    account = await prisma.host.findUnique({ where: { username } })
-    if(account){
-        if(account.password === password){
-            const token = jwt.sign({ id: account.id, type: "host" }, secretKey, {
-                expiresIn: "1h"
-            });
-            return { role: "host", token }
-        }
-        throw new Error("Invalid credentials")
+  //check hosts
+  account = await prisma.host.findUnique({ where: { username } });
+  if (account) {
+    if (account.password === password) {
+      const token = jwt.sign({ id: account.id, type: "host" }, secretKey, {
+        expiresIn: "1h",
+      });
+      return { role: "host", token };
     }
+    throw new Error("Invalid credentials");
+  }
 
-    //check admins
-    account = await prisma.admin.findUnique({
-        where: { username }
-    })
-    if(account){
-        if(account.password === password){
-            const token = jwt.sign({ id: account.id, type: "admin" } , secretKey, {
-                expiresIn: "1h"
-            });
-            return { role: "admin", token }
-        }
-        throw new Error("Invalid credentials")
+  //check admins
+  account = await prisma.admin.findUnique({
+    where: { username },
+  });
+  if (account) {
+    if (account.password === password) {
+      const token = jwt.sign({ id: account.id, type: "admin" }, secretKey, {
+        expiresIn: "1h",
+      });
+      return { role: "admin", token };
     }
+    throw new Error("Invalid credentials");
+  }
 
-    //No user/host/admin found
-    throw new Error("Invalid credentials")
-}
+  //No user/host/admin found
+  throw new Error("Invalid credentials");
+};
